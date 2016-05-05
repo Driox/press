7e4f839e64134e08fd6d3bfb4f883471715c4074
Adrien <adrien.crovetto@gmail.com>
2016-05-05 16:44:07 +0200

    update lib

--- app/controllers/press/Press.java
+++ app/controllers/press/Press.java
@@ -1,9 +1,12 @@
 package controllers.press;
 
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.io.UnsupportedEncodingException;
+import java.util.zip.GZIPOutputStream;
 
 import org.joda.time.DateTime;
 import org.joda.time.format.DateTimeFormat;
@@ -11,116 +14,159 @@ import org.joda.time.format.DateTimeFormatter;
 
 import play.exceptions.UnexpectedException;
 import play.mvc.Controller;
+import play.mvc.Finally;
 import press.CachingStrategy;
 import press.PluginConfig;
 import press.ScriptCompressedFileManager;
-import press.ScriptCompressor;
 import press.ScriptRequestHandler;
 import press.StyleCompressedFileManager;
-import press.StyleCompressor;
 import press.StyleRequestHandler;
 import press.io.CompressedFile;
 import press.io.FileIO;
 
 public class Press extends Controller {
-    public static final DateTimeFormatter httpDateTimeFormatter = DateTimeFormat
-            .forPattern("EEE, dd MMM yyyy HH:mm:ss 'GMT'");
+	public static final DateTimeFormatter httpDateTimeFormatter = DateTimeFormat
+			.forPattern("EEE, dd MMM yyyy HH:mm:ss 'GMT'");
 
-    public static void getCompressedJS(String key) {
-        key = FileIO.unescape(key);
-        CompressedFile compressedFile = new ScriptCompressedFileManager().getCompressedFile(key);
-        renderCompressedFile(compressedFile, "JavaScript");
-    }
+	public static void getCompressedJS(String key) {
+		key = FileIO.unescape(key);
+		CompressedFile compressedFile = new ScriptCompressedFileManager().getCompressedFile(key);
+		renderCompressedFile(compressedFile, "JavaScript");
+	}
 
-    public static void getCompressedCSS(String key) {
-        key = FileIO.unescape(key);
-        CompressedFile compressedFile = new StyleCompressedFileManager().getCompressedFile(key);
-        renderCompressedFile(compressedFile, "CSS");
-    }
+	public static void getCompressedJSFromConfig(String key) {
+		if (key == null || key.isEmpty()) {
+			key = "js.conf";
+		}
+		CompressedFile compressedFile = new ScriptCompressedFileManager().getCompressedFileFromConfig(key, "js");
+		renderCompressedFile(compressedFile, "JavaScript");
+	}
 
-    private static void renderCompressedFile(CompressedFile compressedFile, String type) {
-        flash.keep();
+	public static void getCompressedCSS(String key) {
+		key = FileIO.unescape(key);
+		CompressedFile compressedFile = new StyleCompressedFileManager().getCompressedFile(key);
+		renderCompressedFile(compressedFile, "CSS");
+	}
 
-        if (compressedFile == null) {
-            renderBadResponse(type);
-        }
+	public static void getCompressedCSSFromConfig(String key) {
+		if (key == null || key.isEmpty()) {
+			key = "css.conf";
+		}
+		CompressedFile compressedFile = new StyleCompressedFileManager().getCompressedFileFromConfig(key, "css");
+		renderCompressedFile(compressedFile, "CSS");
+	}
 
-        InputStream inputStream = compressedFile.inputStream();
+	private static void renderCompressedFile(CompressedFile compressedFile, String type) {
+		flash.keep();
 
-        // This seems to be buggy, so instead of passing the file length we
-        // reset the input stream and allow play to manually copy the bytes from
-        // the input stream to the response
-        // renderBinary(inputStream, compressedFile.name(),
-        // compressedFile.length());
+		if (compressedFile == null) {
+			renderBadResponse(type);
+		}
 
-        try {
-            if (inputStream.markSupported()) {
-                inputStream.reset();
-            }
-        } catch (IOException e) {
-            throw new UnexpectedException(e);
-        }
+		InputStream inputStream = compressedFile.inputStream();
 
-        // If the caching strategy is always, the timestamp is not part of the
-        // key. If we let the browser cache, then the browser will keep holding
-        // old copies, even after changing the files at the server and
-        // restarting the server, since the key will stay the same.
-        // If the caching strategy is never, we also don't want to cache at the
-        // browser, for obvious reasons.
-        // If the caching strategy is Change, then the modified timestamp is a
-        // part of the key, so if the file changes, the key in the html file
-        // will be modified, and the browser will request a new version. Each
-        // version can therefore be cached indefinitely.
-        if (PluginConfig.cache.equals(CachingStrategy.Change)) {
-            // Cache for a year
-            response.setHeader("Cache-Control", "max-age=" + 31536000);
-            response.setHeader("Expires", httpDateTimeFormatter.print(new DateTime().plusYears(1)));
-            if(!PluginConfig.p3pHeader.isEmpty()) {
-                response.setHeader("P3P", PluginConfig.p3pHeader);
-            }
-        }
+		// This seems to be buggy, so instead of passing the file length we
+		// reset the input stream and allow play to manually copy the bytes from
+		// the input stream to the response
+		// renderBinary(inputStream, compressedFile.name(),
+		// compressedFile.length());
 
-        renderBinary(inputStream, compressedFile.name());
-    }
+		try {
+			if (inputStream.markSupported()) {
+				inputStream.reset();
+			}
+		} catch (IOException e) {
+			throw new UnexpectedException(e);
+		}
 
-    public static void clearJSCache() {
-        if (!PluginConfig.cacheClearEnabled) {
-            forbidden();
-        }
+		// If the caching strategy is always, the timestamp is not part of the
+		// key. If we let the browser cache, then the browser will keep holding
+		// old copies, even after changing the files at the server and
+		// restarting the server, since the key will stay the same.
+		// If the caching strategy is never, we also don't want to cache at the
+		// browser, for obvious reasons.
+		// If the caching strategy is Change, then the modified timestamp is a
+		// part of the key, so if the file changes, the key in the html file
+		// will be modified, and the browser will request a new version. Each
+		// version can therefore be cached indefinitely.
+		if (PluginConfig.cache.equals(CachingStrategy.Change)) {
+			// Cache for a year
+			response.setHeader("Cache-Control", "max-age=" + 31536000);
+			response.setHeader("Expires", httpDateTimeFormatter.print(new DateTime().plusYears(1)));
+			if (!PluginConfig.p3pHeader.isEmpty()) {
+				response.setHeader("P3P", PluginConfig.p3pHeader);
+			}
+		}
 
-        int count = ScriptRequestHandler.clearCache();
-        renderText("Cleared " + count + " JS files from cache");
-    }
+		renderBinary(inputStream, compressedFile.name());
+	}
 
-    public static void clearCSSCache() {
-        if (!PluginConfig.cacheClearEnabled) {
-            forbidden();
-        }
+	public static void clearJSCache() {
+		if (!PluginConfig.cacheClearEnabled) {
+			forbidden();
+		}
 
-        int count = StyleRequestHandler.clearCache();
-        renderText("Cleared " + count + " CSS files from cache");
-    }
+		int count = ScriptRequestHandler.clearCache();
+		renderText("Cleared " + count + " JS files from cache");
+	}
 
-    private static void renderBadResponse(String fileType) {
-        String response = "/*\n";
-        response += "The compressed " + fileType + " file could not be generated.\n";
-        response += "This can occur in two situations:\n";
-        response += "1. The time between when the page was rendered by the ";
-        response += "server and when the browser requested the compressed ";
-        response += "file was greater than the timeout. (The timeout is ";
-        response += "currently configured to be ";
-        response += PluginConfig.compressionKeyStorageTime + ")\n";
-        response += "2. There was an exception thrown while rendering the ";
-        response += "page.\n";
-        response += "*/";
-        renderBinaryResponse(response);
-    }
+	public static void clearCSSCache() {
+		if (!PluginConfig.cacheClearEnabled) {
+			forbidden();
+		}
 
-    private static void renderBinaryResponse(String response) {
-        try {
-            renderBinary(new ByteArrayInputStream(response.getBytes("UTF-8")));
-        } catch (UnsupportedEncodingException e) {
-            throw new UnexpectedException(e);
-        }
-    }
+		int count = StyleRequestHandler.clearCache();
+		renderText("Cleared " + count + " CSS files from cache");
+	}
+
+	private static void renderBadResponse(String fileType) {
+		String response = "/*\n";
+		response += "The compressed " + fileType + " file could not be generated.\n";
+		response += "This can occur in two situations:\n";
+		response += "1. The time between when the page was rendered by the ";
+		response += "server and when the browser requested the compressed ";
+		response += "file was greater than the timeout. (The timeout is ";
+		response += "currently configured to be ";
+		response += PluginConfig.compressionKeyStorageTime + ")\n";
+		response += "2. There was an exception thrown while rendering the ";
+		response += "page.\n";
+		response += "*/";
+		renderBinaryResponse(response);
+	}
+
+	private static void renderBinaryResponse(String response) {
+		try {
+			renderBinary(new ByteArrayInputStream(response.getBytes("UTF-8")));
+		} catch (UnsupportedEncodingException e) {
+			throw new UnexpectedException(e);
+		}
+	}
+
+	@Finally
+	public static void compress() throws IOException {
+		String text = response.out.toString();
+
+		final ByteArrayOutputStream gzip = gzip(text);
+		response.setHeader("Content-Encoding", "gzip");
+		response.setHeader("Content-Length", gzip.size() + "");
+		response.out = gzip;
+	}
+
+	private static ByteArrayOutputStream gzip(final String input)
+			throws IOException {
+		final InputStream inputStream = new ByteArrayInputStream(input.getBytes());
+		final ByteArrayOutputStream stringOutputStream = new ByteArrayOutputStream((int) (input.length() * 0.75));
+		final OutputStream gzipOutputStream = new GZIPOutputStream(stringOutputStream);
+
+		final byte[] buf = new byte[5000];
+		int len;
+		while ((len = inputStream.read(buf)) > 0) {
+			gzipOutputStream.write(buf, 0, len);
+		}
+
+		inputStream.close();
+		gzipOutputStream.close();
+
+		return stringOutputStream;
+	}
 }
--- app/press/CompressedFileManager.java
+++ app/press/CompressedFileManager.java
@@ -1,50 +1,86 @@
 package press;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
-import play.vfs.VirtualFile;
 import press.io.CompressedFile;
 
 public abstract class CompressedFileManager {
-    private PressFileWriter pressFileWriter;
-    private Compressor compressor;
+	private PressFileWriter pressFileWriter;
+	private Compressor compressor;
 
-    public CompressedFileManager(Compressor compressor) {
-        this.compressor = compressor;
-        this.pressFileWriter = new PressFileWriter(compressor);
-    }
+	public CompressedFileManager(Compressor compressor) {
+		this.compressor = compressor;
+		this.pressFileWriter = new PressFileWriter(compressor);
+	}
 
-    /**
-     * Get the compressed file with the given compression key
-     */
-    public CompressedFile getCompressedFile(String key) {
-        List<FileInfo> componentFiles = SourceFileManager.getSourceFiles(key);
+	/**
+	 * Get the compressed file with the given compression key
+	 */
+	public CompressedFile getCompressedFile(String key) {
+		List<FileInfo> componentFiles = SourceFileManager.getSourceFiles(key);
 
-        // If there was nothing found for the given request key, return null.
-        // This shouldn't happen unless there was a very long delay between the
-        // template being rendered and the compressed file being requested
-        if (componentFiles == null) {
-            return null;
-        }
+		// If there was nothing found for the given request key, return null.
+		// This shouldn't happen unless there was a very long delay between the
+		// template being rendered and the compressed file being requested
+		if (componentFiles == null) {
+			return null;
+		}
 
-        return getCompressedFile(componentFiles);
-    }
+		return getCompressedFile(componentFiles);
+	}
 
-    /**
-     * Get the compressed file for the given set of component files
-     */
-    public CompressedFile getCompressedFile(List<FileInfo> componentFiles) {
-        // First check if the compressor has a cached copy of the file
-        String key = compressor.getCompressedFileKey(componentFiles);
-        CompressedFile file = CompressedFile.create(key, getCompressedDir());
-        if (CacheManager.useCachedFile(file)) {
-            return file;
-        }
+	public CompressedFile getCompressedFileFromConfig(String key, String type) {
+		List<FileInfo> componentFiles = buildDefaultFileInfo(key, type);
 
-        // If there is no cached file, generate one
-        return pressFileWriter.writeCompressedFile(componentFiles, file);
-    }
-    
-    public abstract String getCompressedDir();
+		// If there was nothing found for the given request key, return null.
+		// This shouldn't happen unless there was a very long delay between the
+		// template being rendered and the compressed file being requested
+		if (componentFiles == null) {
+			return null;
+		}
+
+		return getCompressedFile(componentFiles);
+	}
+
+	private List<FileInfo> buildDefaultFileInfo(String key, String type) {
+		SourceFileManager manager;
+		if (type.equals("css")) {
+			manager = new StyleFileManager();
+		} else if (type.equals("js")) {
+			manager = new ScriptSourceFileManager();
+		} else {
+			return null;
+		}
+
+		boolean compress = false;
+		List<FileInfo> file_infos = new ArrayList();
+		for (String file_name : loadFiles(key)) {
+			file_infos.add(new FileInfo(compress, manager.checkFileExists(file_name)));
+		}
+
+		return file_infos;
+	}
+
+	private List<String> loadFiles(String type) {
+		return Arrays.asList(play.vfs.VirtualFile.fromRelativePath("/conf/" + type).contentAsString().split("\n"));
+	}
+
+	/**
+	 * Get the compressed file for the given set of component files
+	 */
+	public CompressedFile getCompressedFile(List<FileInfo> componentFiles) {
+		// First check if the compressor has a cached copy of the file
+		String key = compressor.getCompressedFileKey(componentFiles);
+		CompressedFile file = CompressedFile.create(key, getCompressedDir());
+		if (CacheManager.useCachedFile(file)) {
+			return file;
+		}
+
+		// If there is no cached file, generate one
+		return pressFileWriter.writeCompressedFile(componentFiles, file);
+	}
+
+	public abstract String getCompressedDir();
 }
